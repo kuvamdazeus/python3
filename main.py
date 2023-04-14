27a1c5d6c50cc74fb14021a1ce4fe74098d39ee5
Kuvam Bhardwaj <kuvamyash@gmail.com>
2023-04-14 18:42:34 +0530

    - something message

---
+++ main.py
@@ -0,0 +1,25 @@
+import re
+
+f = open("input.txt")
+
+content = f.read()
+
+for testimonial in content.split("\n\n"):
+    text, by = testimonial.split("\n")
+    by = by.replace("[", "").replace("]", "")
+    [name, designation, company_name] = by.split(", ")
+
+    print("""
+    <div className="col-md-12">
+        <div className="card-testimonial text-center">
+            <p>
+                {}
+            </p>
+            <span className="client-detail text-white">
+                {}, {}, <span className="text-style-2 ">{}</span>
+            </span>
+        </div>
+    </div>
+    """.format(text, name, designation, company_name))
+
+    print()
